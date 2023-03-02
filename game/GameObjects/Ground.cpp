7ec1d840604d815a532d804c05faf509bd4379be
Enzo GUILMER <enzo.guilmer@gmail.com>
2023-03-02 21:39:14 +0100

    fix: ground

--- game/GameObjects/Ground.cpp
+++ game/GameObjects/Ground.cpp
@@ -6,16 +6,17 @@ Ground::Ground(std::vector<Vec2>& vertices, Vec2& position) {
 	m_body = EntityFactory::create<PolygonEntity>(vertices, position);
 	thor::ConcaveShape concaveShape;
 
-	concaveShape.setPointCount(12);
+	concaveShape.setPointCount(vertices.size());
     for (auto i = 0; i < vertices.size(); ++i)
     {
         auto& v = vertices[i];
         concaveShape.setPoint(i, sf::Vector2f(v.x, v.y));
-
     }
 
     concaveShape.setFillColor(sf::Color::White);
     concaveShape.setPosition(position.x, position.y);
+
+    m_shape = concaveShape;
 
 	std::cout << "Ground créé" << std::endl;
 }
